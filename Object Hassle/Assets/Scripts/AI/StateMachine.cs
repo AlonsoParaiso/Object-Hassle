a85625c70b5e8adefa12b14143fd0f97a9b1558d
ElPilas1 <dieya2003@gmail.com>
2024-10-21 10:38:30 +0200

    ia

--- Object Hassle/Assets/Scripts/AI/StateMachine.cs
+++ Object Hassle/Assets/Scripts/AI/StateMachine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class StateMachine : MonoBehaviour
 {
@@ -21,7 +22,6 @@ public class StateMachine : MonoBehaviour
         {
             currentState = nextstate;
         }
-
     }
 
     private void OnDrawGizmos()
---
+++ Object Hassle/Assets/Scripts/IA_Movement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Movement : MonoBehaviour
+{
+    public float speed, jumpForce;
+    public int maxJumps;
+
+    public GameObject player;
+
+    private int currentJumps = 0;
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.transform.position.x, 1.5f, transform.position.z), speed * Time.deltaTime);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("ground"))
+        {
+            StopCoroutine(kk());
+            StartCoroutine(kk());
+        }
+    }
+
+    IEnumerator kk()
+    {
+        yield return new WaitForSeconds(.5f);
+        for(currentJumps = 0; currentJumps < maxJumps; currentJumps++)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(-5, jumpForce, 0), ForceMode.Impulse);
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+}
