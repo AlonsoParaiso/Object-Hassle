52ea85525ef06010a775f549bd212d9124c442b8
D Yanes <dieya2003@gmail.com>
2025-02-06 14:14:08 +0100

    Knockback mal

--- Object Hassle/Assets/Scripts/Knockback.cs
+++ Object Hassle/Assets/Scripts/Knockback.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Knockback : MonoBehaviour
 {
+    private Rigidbody rb;
+    private Character character;
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb=GetComponent<Rigidbody>(); 
+        character=GetComponent<Character>();
     }
 
     // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ()
+            rb.AddForce((transform.forward + Vector3.up) * character.GetDamage());
+    }
     void Update()
     {
-        
+       
     }
 }
