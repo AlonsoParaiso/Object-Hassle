8a06d2453ab4b0c86b51606a512e7bb5080726c6
ElPilas1 <154227496+ElPilas1@users.noreply.github.com>
2025-05-23 11:06:44 +0200

    fixed Pun characters
    Co-Authored-By: AlonsoParaiso <83759409+AlonsoParaiso@users.noreply.github.com>
    Co-Authored-By: DiegoZC <153915013+DiegoZurroooCobo@users.noreply.github.com>

--- Object Hassle/Assets/Scripts/Photon/PlayerMovementPun.cs
+++ Object Hassle/Assets/Scripts/Photon/PlayerMovementPun.cs
@@ -82,7 +82,7 @@ public class PlayerMovementPun : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        if (!photonView.IsMine && PhotonNetwork.IsConnected)
+        if (photonView.IsMine && PhotonNetwork.IsConnected)
         {
             return;
         }
